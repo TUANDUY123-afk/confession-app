75c0dafc3258013c4a3b952d4674741bb8f037b8
TUANDUY123 <dndc2pp98@gmail.com>
2025-10-27 00:02:50 +0700

    Bump version to 0.1.3

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import FloatingHearts from "@/components/floating-hearts"
 import ConfessionMessage from "@/components/confession-message"
 import Link from "next/link"
 
-const APP_VERSION = "v0.1.2"
+const APP_VERSION = "v0.1.3"
 
 export default function Home() {
   const [showMessage, setShowMessage] = useState(false)
